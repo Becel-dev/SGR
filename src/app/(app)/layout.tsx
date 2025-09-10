5351c2dbd4befb95b5736b1d3f52996ba2a5dc13
Pedro Marcelo Becel <marcelobecel@gmail.com>
2025-09-10 19:14:29 +0000

    Continua com o problema, o menu deve se manter fixo na área de visualiza

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -7,7 +7,7 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <AppSidebar />
       <div className="flex flex-col">
         <AppHeader />
