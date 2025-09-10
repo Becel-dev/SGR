94a8ea1d11d76a1d7f64f8159b7c261b98ea97c8
Pedro Marcelo Becel <marcelobecel@gmail.com>
2025-09-10 19:21:20 +0000

    Travar a posição do menu na área de visualização, não permitindo a rolag

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -9,7 +9,7 @@ export default function AppLayout({
   return (
     <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <AppSidebar />
-      <div className="flex flex-col">
+      <div className="flex flex-col overflow-hidden">
         <AppHeader />
         <main className="flex flex-1 flex-col gap-4 overflow-auto bg-muted/40 p-4 lg:gap-6 lg:p-6">
           {children}
