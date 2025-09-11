0ca5d83c83e52f3c019cbd868554a849258b14ef
Pedro Marcelo Becel <marcelobecel@gmail.com>
2025-09-11 18:20:21 +0000

    Esses itens não estão sendo usados, pode removelos?

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -1,22 +1,10 @@
 
 'use client';
 
-import { Menu, Search } from 'lucide-react';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Input } from '@/components/ui/input';
+import { Menu } from 'lucide-react';
 import { UserMenu } from '@/components/auth/user-menu';
 import { Button } from '@/components/ui/button';
 import { useSidebar } from '@/hooks/use-sidebar';
-import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
-import { AppSidebar } from './app-sidebar';
-
 
 export function AppHeader() {
   const { toggleSidebar } = useSidebar();
@@ -43,29 +31,8 @@ export function AppHeader() {
           <span className="sr-only">Toggle navigation menu</span>
         </Button>
 
-
-       <Breadcrumb className="hidden md:flex">
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/dashboard">Portal</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Dashboard</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
       <div className="w-full flex-1">
-        <form>
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Buscar..."
-              className="w-full appearance-none bg-card pl-8 shadow-none md:w-2/3 lg:w-1/3"
-            />
-          </div>
-        </form>
+        {/* Espaço reservado para futuros elementos */}
       </div>
       <UserMenu />
     </header>
